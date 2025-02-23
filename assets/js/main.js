d6184d9cada8ce70150de28092b929211cd3d7ce
Troy Gamble <troy.gamble@outlook.com>
2025-02-23 15:18:45 +1100

    all working 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
 function getRandomQuestion() {
-    var questions = [
-      {% for question in site.questions %}
-        "{{ question.url }}",
-      {% endfor %}
-    ];
-    return questions[Math.floor(Math.random() * questions.length)];
+    const questions = window.siteQuestions || [];
+    if (questions.length > 0) {
+        window.location.href = questions[Math.floor(Math.random() * questions.length)];
+    }
 }
